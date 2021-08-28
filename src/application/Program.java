f20e9b52bcc789bb00ffd0889f845cf2069fee78
Alan Ribeiro <alan.ribe@gmail.com>
2021-08-28 11:12:17 -0300

    Fix - better treatment to promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -40,8 +41,12 @@ public class Program {
 					captured.add(capturedPiece);
 				}
 				if (chessMatch.getPromoted() != null) {
-					System.out.print("Enter piece for promotion |Q|R|N|B| ");
-					String type = sc.nextLine();
+					System.out.print("Enter piece for promotion (Q|R|N|B): ");
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Invalid value! Enter piece for promotion (Q|R|N|B): ");
+						type = sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 		
